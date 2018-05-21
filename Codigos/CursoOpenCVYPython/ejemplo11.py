86394db9d50f23cc3684a113f26fde67caec56bd
Mario Merlo <mariomr289@gmail.com>
2018-05-21 17:50:42 -0500

    Ejemplo 11. Filtros de Sobel.

---
+++ Codigos/CursoOpenCVYPython/ejemplo11.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+# Load image
+image = cv2.imread('examples/images/bike_sobel.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Sobel
+k = 3
+sobelx = cv2.Sobel(image, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=k)
+sobely = cv2.Sobel(image, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=k)
+
+# Show
+cv2.imshow('Original', image)
+cv2.imshow('Sobel X', sobelx)
+cv2.imshow('Sobel Y', sobely)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
