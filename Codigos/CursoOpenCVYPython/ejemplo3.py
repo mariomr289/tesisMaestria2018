173f6d63f2e234a8934e33753feff76bee69241f
Mario Merlo <mariomr289@gmail.com>
2018-05-21 12:52:56 -0500

    Ejemplo 3. Cargar y reproducir un vídeo en escala de grises.

---
+++ Codigos/CursoOpenCVYPython/ejemplo3.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+video_file = 'examples/videos/Bubble_Turning_Into_Ice.mp4'
+cap = cv2.VideoCapture(video_file)
+
+while (cap.isOpened()):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    if ret == True:
+        # Operations on the frame
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        cv2.imshow('frame', gray)
+
+        # Exit?
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
