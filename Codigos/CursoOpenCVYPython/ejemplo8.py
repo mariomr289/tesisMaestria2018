c7b8fbdcb463005b6633c034315d4e8cfd784f30
Mario Merlo <mariomr289@gmail.com>
2018-05-21 17:30:23 -0500

    Ejemplo 8. Filtrado de imagen: filtro Gaussiano.

---
+++ Codigos/CursoOpenCVYPython/ejemplo8.py
@@ -0,0 +1,16 @@
+import cv2
+
+# Load image
+image_path = 'examples/images/sunset.jpg'
+image = cv2.imread(image_path)
+
+# Gaussian blur
+k = 5
+sigma = 0
+blur = cv2.GaussianBlur(image, (k, k), sigma)
+
+# Show
+cv2.imshow('Original', image)
+cv2.imshow('Filtered', blur)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
