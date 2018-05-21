b18af9fe70b19c9e5a397ff30bbd1b79ef311fbb
Mario Merlo <mariomr289@gmail.com>
2018-05-21 18:30:49 -0500

    Ejemplo 13. Calcular el histograma sobre una imagen en escala de grises y mostrar el gráfico.

---
+++ Codigos/CursoOpenCVYPython/ejemplo13.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Load
+image_path = 'examples/images/rabbit.jpg'
+image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+# Show image
+cv2.imshow('Image', image)
+cv2.waitKey(0)
+
+# Histogram
+hist = cv2.calcHist([image], channels=[0], mask=None, histSize=[256], ranges=[0,255])
+
+# Plot
+plt.plot(hist)
+plt.xlim([0, 255])
+plt.show()
+
+cv2.destroyAllWindows()
