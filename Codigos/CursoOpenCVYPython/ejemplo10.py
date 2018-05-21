4933ab01bca4ad12814d1fee315f44e204d4b604
Mario Merlo <mariomr289@gmail.com>
2018-05-21 17:42:34 -0500

    Ejemplo 10. Filtrado de imagen: filtro mediana sobre imagen con ruido de tipo "sal y pimienta".

--- Codigos/CursoOpenCVYPython/ejemplo10.py
+++ Codigos/CursoOpenCVYPython/ejemplo10.py
@@ -1,16 +1,15 @@
 import cv2
 
 # Load image
-image_path = 'examples/images/ruido.jpg'
+image_path = 'examples/images/Noise_salt_and_pepper.png'
 image = cv2.imread(image_path)
 
 # Gaussian blur
 k = 5
-sigma = 1
-blur = cv2.GaussianBlur(image, (k, k), sigma)
+blur = cv2.medianBlur(image, k)
 
 # Show
-cv2.imshow('Original with noise', image)
+cv2.imshow('Original', image)
 cv2.imshow('Filtered', blur)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
