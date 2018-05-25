8368a51d5f5fdd7c5819058c9767cfc04aa1d260
Mario Merlo <mariomr289@gmail.com>
2018-05-25 12:10:39 -0500

    Ejemplo 19. Detección de esquinas mediante el método de Harris.

---
+++ Codigos/CursoOpenCVYPython/ejemplo19.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+filename = 'examples/images/taj-mahal-1.png'
+img = cv2.imread(filename)
+
+# RGB -> Grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Input image should be grayscale and float32 type
+gray = np.float32(gray)
+
+# Corner detection
+dst = cv2.cornerHarris(gray, blockSize=2, ksize=3, k=0.04)
+
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.01*dst.max()]=[0,0,255]
+
+# Show
+cv2.imshow('dst', img)
+
+# Exit?
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
